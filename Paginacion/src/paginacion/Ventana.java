d0a53c7bc40bf91feb9d33c3837c2bb89e189a00
Chepe Aicrag <48169157+ChepeAicrag12@users.noreply.github.com>
2019-11-15 14:53:25 -0600

    Terminado
    Afinar detalles

--- Paginacion/src/paginacion/Ventana.java
+++ Paginacion/src/paginacion/Ventana.java
@@ -6,70 +6,105 @@
 package paginacion;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 public class Ventana extends JFrame {
-    public Ventana(){
+    private JPanel panel;
+    private int tamArchivo;
+    private int tamPagina;
+    
+    public Ventana(int tamArchivo, int tamPagina){
+        JScrollPane deslizador = new JScrollPane();
+        deslizador.setBounds(15, 30, 2000,2000);
+        this.tamArchivo = tamArchivo;
+        this.tamPagina = tamPagina;
         this.setSize(800,800);
         this.setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setTitle("PAGINACION DE MEMORIA");
         this.setLocationRelativeTo(null);
-        //iniciarComponentes();
+        panel = new JPanel();
+        panel.setLayout(null);
+        panel.setBackground(Color.black);
+        panel.setPreferredSize(new Dimension(1080,1080));
+        deslizador.setViewportView(panel);
+        //panel.setPreferredSize(new Dimension(5000,5000));
+        this.getContentPane().add(deslizador);
         crearPaginas();
         
   }
-    private void iniciarComponentes(){
-        JPanel panel = new JPanel();
-        panel.setBackground(Color.black);
-        this.getContentPane().add(panel);
-        JLabel etiqueta = new JLabel("Tamaño del archivo");
-        etiqueta.setLayout(null);
-        etiqueta.setBounds(50, 50, 150, 30);
-        JLabel etiqueta2 = new JLabel("Numero de paginas");
-        etiqueta2.setLayout(null);
-        etiqueta2.setBounds(50, 90, 150, 30);
-        panel.add(etiqueta);
-        panel.add(etiqueta2);
-    }   
+ private void Datos(){
+    /*
+    JLabel dato1 = new JLabel("Tamaño del archivo");
+    dato1.setLayout(null);
+    dato1.setBounds(100, 10, 200, 100);
+    
+    JLabel dato2 = new JLabel("Tamaño de la pagina");
+    dato2.setLayout(null);
+    dato2.setBounds(500, 10, 200, 100);
+    panel.add(dato1);
+    panel.add(dato2);
+    
+    */
+    JTextField numDato1 = new JTextField("Tamaño del archivo");
+    numDato1.setBounds(100, 500, 100, 30);
+    JTextField numDato2 = new JTextField("Tamaño de la pagina");
+    numDato2.setBounds(500, 500, 100, 30);
+    panel.add(numDato1);
+    panel.add(numDato2);
+    //tamArchivo = Integer.parseInt(numDato1.getText());
+    //tamPagina = Integer.parseInt(numDato2.getText());
+    JButton boton1 = new JButton("Inciar");
+    boton1.setBounds(250, 50, 50, 50);
+    panel.add(boton1);
     
     
-    private void crearPaginas(){
-        JPanel panel = new JPanel();
-        panel.setBackground(Color.black);
-        this.getContentPane().add(panel);
-        float x = 5;
+}
+    
+    
+private void crearPaginas(){
+        
+        float x = (float) tamArchivo/tamPagina;
         int cont = 1;
-        int posY = 50;
+        int posX = 100;
+        int posY = 80;
         while (cont <= x) {            
             
-            JLabel hojaFisica = new JLabel("Pagina Fisica " + cont,SwingConstants.CENTER);
+            // Creación de la hoja fisica
+            JLabel hojaFisica = new JLabel("Pagina Fisica " + cont + " " + x/1 + " mb",SwingConstants.CENTER);
             hojaFisica.setOpaque(true);
-            hojaFisica.setBackground(Color.red);
-            hojaFisica.setBounds(300,posY,150,30);
+            hojaFisica.setBackground(new Color((int) (Math.random()*255),(int) (Math.random()*255),(int) (Math.random()*255)));
+            hojaFisica.setBounds(posX,posY,150,30);
             
-            JLabel hojaVirtual = new JLabel("Pagina Virtual " + cont,SwingConstants.CENTER);
+            // Creación de la hoja virtual
+            JLabel hojaVirtual = new JLabel("Pagina Virtual " + cont + " " + x/1 + " mb",SwingConstants.CENTER);
             hojaVirtual.setOpaque(true);
-            hojaVirtual.setBackground(Color.BLUE);
-            hojaVirtual.setBounds(300,posY + 40,150,30);
-            
+            hojaVirtual.setBackground(new Color((int) (Math.random()*255),(int) (Math.random()*255),(int) (Math.random()*255)));
+            hojaVirtual.setBounds(posX,posY + 40,150,30);
             
             cont++;
             posY += 100;
             panel.add(hojaFisica);
             panel.add(hojaVirtual);
+            
+            if(cont % 10 == 0){
+                posX = 300;
+                posY = 80;
+            }
         }
         if(x % 1 != 0){
-            JLabel hojaFisica = new JLabel("Pagina Fisica " + cont,SwingConstants.CENTER);
+            JLabel hojaFisica = new JLabel("Pagina Fisica " + cont + " " + x/1 + " mb" ,SwingConstants.CENTER);
             hojaFisica.setOpaque(true);
-            hojaFisica.setBackground(Color.red);
-            hojaFisica.setBounds(300,posY,150,30);
+            hojaFisica.setBackground(new Color((int) (Math.random()*255),(int) (Math.random()*255),(int) (Math.random()*255)));
+            hojaFisica.setBounds(posX,posY,150,30);
             panel.add(hojaFisica);
         }
-        
     }
-        
 }
